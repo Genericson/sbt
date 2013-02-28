5855ad801983939c5123280811f863a7d51c9ffb
Harrison Leadlay <sumbeard@not.myemail>
2013-02-28 00:28:19 -0700

    added vec.hpp and vec.inl

---
+++ vec.hpp
@@ -0,0 +1,168 @@
+//====================LICENSE====================//
+// Copyright (c) 2013, Harrison Leadlay
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//  1) Redistributions of source code must retain the above copyright notice,
+//     this list of conditions and the following disclaimer.
+//
+//  2) Redistributions in binary form must reproduce the above copyright notice,
+//     this list of conditions and the following disclaimer in the documentation
+//     and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+// OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+// ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//===============================================//
+
+/**
+ * \mainpage Sumbeard's Tools (sbt)
+ *
+ * \version    0.1a
+ */
+
+#ifndef vec_HPP_
+#define vec_HPP_
+
+/** \namespace sbt Sumbeard's Tools */
+namespace sbt {
+
+template <typename T, unsigned int L>
+
+class vec {
+/** A vector (as in mathematics and physics) template class
+*/
+private:
+    const static unsigned int len = L;
+    T d[L];
+public:
+    vec();
+    vec (const T value);
+    vec (const vec<T, L>& v);
+
+    vec (const T v[L]);
+    /** Access component directly (via reference).
+    *   \param [out] T returns reference to component at index
+    *   \param [in]  index index of component (starting from 0)
+    *   \warning This method does not check the index to be in bounds
+    */
+    T& operator[] (const unsigned int index);
+    /** Access component directly (via reference) read-only.
+    *   \param [out] T returns reference to component at index
+    *   \param [in]  index index of component (starting from 0)
+    *   \warning This method does not check the index to be in bounds
+    */
+    const T& operator[] (const unsigned int index) const;
+    /** Returns the number of components/dimensions of the vector
+    *   \param [out] uint length
+    */
+    const unsigned int length() const;
+    //-----------OPERATORS------------//
+    /** Copy vector by component
+    *
+    */
+    const vec& operator= (const vec& v);
+    /** Copy array by component
+    *
+    */
+    const vec& operator= (const T v[L]);
+    /** Multiply vectors component-component
+    *
+    */
+
+    const vec operator* (const vec<T, L>& v) const;
+    /** Multiple vector by scalar */
+    const vec operator* (const T& s) const;
+    /** Equals comparison
+    *   \return returns `true` if vectors are equal component-wise
+    *       `false` if any one set of components are not equal
+    *
+    *   <1, 2, 3> == <1, 2, 3> == true
+    *   <2, 1, 3> == <1, 2, 3> == false
+    *
+    *   \warning Consider floating-point error when using such types of vectors
+    */
+    bool operator== (const vec<T, L>& v) const;
+    /** Calculates the norm/magnitude of the vector
+    *   \param [out] the norm
+    */
+    T norm() const;
+    /** Destructor */
+    ~vec();
+
+    //===============STATIC FUNCTIONS================//
+    static T dot(const vec<T, L>& a, const vec<T, L>& b);
+
+    static vec<T, 3u> cross(const vec<T, 3u>& a, const vec<T, 3u>& b);
+
+}; //class vec
+
+//=============Aliases ==============//
+
+    /** \namespace fvec
+    *   \brief single-precision floating point vectors
+    *
+    *  This name space includes separate classes for `float`
+    *  type 2-4 dimensional vectors
+    */
+    namespace fvec {
+    using vec2 = vec<float, 2u>;
+    using vec3 = vec<float, 3u>;
+    using vec4 = vec<float, 4u>;
+    }
+    /** \namespace dvec
+    *   \brief double-precision floating point vectors
+    *
+    *  This name space includes separate classes for `double`
+    *  type 2-4 dimensional vectors
+    */
+    namespace dvec {
+    using dvec2 = vec<double, 2u>;
+    using dvec3 = vec<double, 3u>;
+    using dvec4 = vec<double, 4u>;
+    }
+    /** \namespace bvec
+    *   \brief Boolean vectors
+    *
+    *  This name space includes separate classes for `bool`
+    *  type 2-4 dimensional vectors
+    */
+    namespace bvec {
+    using bvecd2 = vec<bool, 2u>;
+    using bvecd3 = vec<bool, 3u>;
+    using bvecd4 = vec<bool, 4u>;
+    }
+    /** \namespace ivec
+    *   \brief Signed and unsigned integer vectors
+
+    *  This name space includes separate classes for signed and unsigned
+    *  `int`
+    *  type 2-4 dimensional vectors
+    */
+    namespace ivec {
+    using ivec2 = vec<int, 2u>;
+    using ivec3 = vec<int, 3u>;
+    using ivec4 = vec<int, 4u>;
+    using uvec2 = vec<unsigned int, 2u>;
+    using uvec3 = vec<unsigned int, 3u>;
+    using uvec4 = vec<unsigned int, 4u>;
+    }
+
+
+} //namespace sbt
+
+//=============INCLUDE IMPLEMENTATION==============//
+
+#include "vec.inl"
+
+#endif vec_HPP_
---
+++ vec.inl
@@ -0,0 +1,153 @@
+//====================LICENSE====================//
+// Copyright (c) 2013, Harrison Leadlay
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//  1) Redistributions of source code must retain the above copyright notice,
+//     this list of conditions and the following disclaimer.
+//
+//  2) Redistributions in binary form must reproduce the above copyright notice,
+//     this list of conditions and the following disclaimer in the documentation
+//     and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+// OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+// ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//===============================================//
+
+#include <iostream>
+#include <string>
+#include <cmath>
+
+namespace sbt {
+
+template <typename T, unsigned int L>
+vec<T, L>::vec () {
+    for(unsigned int i; i < length(); i++)
+        d[i] = 0;
+} //vec()
+
+template <typename T, unsigned int L>
+vec<T, L>::vec (const T value) {
+    for(unsigned int i = 0u; i < length(); i++)
+        d[i] =  value;
+} //vec(T)
+
+template <typename T, unsigned int L>
+vec<T, L>::vec (const vec<T, L>& v) {
+    for(unsigned int i = 0u; i < length(); i++)
+        d[i] = v[i];
+} //vec(vec)
+
+template <typename T, unsigned int L>
+vec<T, L>::vec (const T v[L]) {
+    for(unsigned int i = 0; i < length(); i++)
+        d[i] = v[i];
+} //vec(T[L])
+
+template <typename T, unsigned int L>
+T& vec<T, L>::operator[] (const unsigned int index) {
+    return d[index];
+} //operator[](uint);
+
+template <typename T, unsigned int L>
+const T& vec<T, L>::operator[] (const unsigned int index) const {
+    return d[index];
+} //operator[](uint);
+
+template <typename T, unsigned int L>
+const unsigned int vec<T, L>::length() const {
+    return this->len;
+} //length()
+
+template <typename T, unsigned int L>
+const vec<T, L>& vec<T, L>::operator= (const vec& v) {
+    for(unsigned int i = 0; i < length(); i++)
+        d[i] = v[i];
+    return *this;
+} //operator=(vec)
+
+template <typename T, unsigned int L>
+const vec<T, L>& vec<T, L>::operator= (const T v[L]) {
+    for(unsigned int i = 0; i < length(); i++)
+        d[i] = v[i];
+    return *this;
+} //operator=(T[L])
+
+template <typename T, unsigned int L>
+const vec<T, L> vec<T, L>::operator* (const vec<T, L>& v) const {
+    vec<T, L> temp;
+    for(unsigned int i = 0; i < length(); i++)
+        temp[i] = v[i] * d[i];
+    return temp;
+} //operator*(vec)
+
+template <typename T, unsigned int L>
+const vec<T, L> vec<T, L>::operator* (const T& s) const {
+    vec<T, L> temp;
+    for(unsigned int i = 0; i < length(); i++)
+        temp[i] = d[i] * s;
+    return temp;
+} //operator*(T)
+
+template <typename T, unsigned int L>
+bool vec<T, L>::operator== (const vec<T, L>& v) const {
+    unsigned int i = 0;
+    // uses loop conditional to check if each set of components are equal
+    for(; i < length() && d[i] == v[i]; i++);
+    // returns true if loop completed (i < length() terminated loop,
+    //     all component comparisons equal)
+    // returns false if loop terminated (d[i] == v[i] terminated the loop,
+    //     at least one set of components not equal)
+    return i == length();
+} //operator==(vec)
+
+template <typename T, unsigned int L>
+T vec<T, L>::norm() const {
+    T result = 0;
+    for(unsigned int i = 0; i < length(); i++) {
+        result = (d[i])*(d[i]) + result;
+        std::cerr<<"result: "<<result<<std::endl;
+    }
+    return std::sqrt(result);
+} //norm()
+template <typename T, unsigned int L>
+vec<T, L>::~vec() {
+
+} //~vec()
+
+//-------------STATIC FUNCTIONS--------------//
+/** Returns dot product of two vectors
+*/
+template <typename T, unsigned int L>
+T dot(const vec<T, L>& a, const vec<T, L>& b) {
+    T result = 0;
+    for(unsigned int i = 0; i < L; i++) {
+        result += a[i] * b[i];
+    }
+    return result;
+} //dot(vec, vec)
+
+/** Cross product for 3-dimensional vectors
+*   \param [out] vector orthogonal to a and b
+*/
+template <typename T, unsigned int L>
+vec<T, 3u> cross(const vec<T, 3u>& a, const vec<T, 3u>& b) {
+    vec<T, L> r;
+    r[0] = a[1]*b[2] - a[2]*b[1];
+    r[1] = a[2]*b[0] - a[0]*b[2];
+    r[2] = a[0]*b[1] - a[1]*b[0];
+    return r;
+} //cross(vec3, vec3)
+
+} //namespace sbt
