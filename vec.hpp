b78aa2b9b299105d3e9e44ff8763cce3a9b120c7
Harrison Leadlay <sumbeard@not.myemail>
2013-03-07 18:59:56 -0700

    General design comments area added

--- vec.hpp
+++ vec.hpp
@@ -29,6 +29,12 @@
 // OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 // ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 /////////////////////////////////////////////////
+//General design comments
+//=============================================//
+//
+//
+//
+/////////////////////////////////////////////////
 
 /////////////////////////////////////////////////
 /// \mainpage Sumbeard's Tools (sbt)
